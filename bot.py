f241ad0c95b8c2a96b2013592da86915a41f956a
Kai <70678154+Rfkgaming89@users.noreply.github.com>
2023-05-03 05:55:35 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+
+TOKEN = 'your_bot_token_here'
+COPY_COMMAND = '!copy'
+COPY_FROM_CHANNEL_ID = 123456789012345678 # Replace with the actual ID of the channel to copy from
+COPY_TO_CHANNEL_ID = 123456789012345678 # Replace with the actual ID of the channel to copy to
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(COPY_COMMAND):
+        channel = client.get_channel(COPY_FROM_CHANNEL_ID)
+        async for msg in channel.history(limit=10):
+            await client.get_channel(COPY_TO_CHANNEL_ID).send(msg.content)
+
+client.run(TOKEN)
